5fe8a19840d051093972a05466771501a9bb3cc5
Andrea <b.a.benvenuti3@gmail.com>
2021-04-08 17:30:06 +0200

    completati bonus 1 e 2

--- js/script.js
+++ js/script.js
@@ -4,12 +4,24 @@ var app = new Vue(
     // -------------------------------------------------------------------------
     data: {
       dischi: [],
+      generi: [],
+      optionGenere: '',
     },
     // -------------------------------------------------------------------------
     mounted: function() {
       axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then((risposta) => {
         this.dischi = risposta.data.response;
+        //BONUS 1: 'Catturo' tutti i generi presenti
+        this.dischi.forEach(disco => {
+          if(!this.generi.includes(disco.genre)) {
+            this.generi.push(disco.genre);
+          }
+        });
+        //BONUS 2: Ordinare i dischi per anno di uscita.
+        this.dischi.sort(function(disco1, disco2) {
+          return parseInt(disco1.year) - parseInt(disco2.year);
+        });
       });
     },
     // -------------------------------------------------------------------------
